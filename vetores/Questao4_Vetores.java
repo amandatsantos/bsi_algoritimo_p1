3260fa0ca5797eea3396d06a2f8fd4230e648be5
Henrique Silva <henrsilvasantos@gmail.com>
2022-11-09 09:35:53 -0300

    alg (vetores) : vetores/09112022

---
+++ vetores/Questao4_Vetores.java
@@ -0,0 +1,24 @@
+
+package questao4_vetores;
+import javax.swing.JOptionPane;
+public class Questao4_Vetores {
+
+ 
+    public static void main(String[] args) {
+        //4.Ler 30 números reais em um vetor e depois exibir os números localizados nas posições impares.
+        double vetor [];
+        vetor = new double [30];
+        String var ="";
+       
+         for (int i=0;i<30;i++){
+            
+            var = JOptionPane.showInputDialog(null,"Digite os numero");
+            vetor[i] = Integer.parseInt(var);
+      }
+        for (int j=0;j<30;j++){
+            if (vetor[j]%2!=1){
+               JOptionPane.showMessageDialog(null,"Os numeros ímpares são: "+vetor[j]);
+            }
+       }
+    }
+}
